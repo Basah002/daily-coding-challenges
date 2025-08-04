1ab4f5cef50f414250bef9a604cb30a0d859a188
Basah002 <129395169+Basah002@users.noreply.github.com>
2025-08-04 22:01:17 +0200

    Update SolutionWithSortedArray.java

--- easy/single-number/SolutionWithSortedArray.java
+++ easy/single-number/SolutionWithSortedArray.java
@@ -4,7 +4,7 @@ class Solution {
         if (nums.length == 1)
             return nums[0];
 
-        int singleNumber = 0;
+       
         
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i += 2) {
